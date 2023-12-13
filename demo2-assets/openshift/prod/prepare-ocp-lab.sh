1bf6dd1795a5228fea8d3a32611d5a7f55def083
juazugas <juazuri@gmail.com>
2023-12-13 15:10:01 +0100

    feat: change namespace naming

---
+++ demo2-assets/openshift/prod/prepare-ocp-lab.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+
+numStudents=${STUDENTS:-50}
+nsPreffix=${STUDET_NS:-student}
+
+OC="oc"
+
+for i in $(seq "${numStudents}"); do 
+  echo "Preparing environment for student $i"
+  namespace="${nsPreffix}${i}"
+  $OC annotate --overwrite namespace/${namespace} 'operator.tekton.dev/prune.keep=2'
+  $OC annotate --overwrite namespace/${namespace} 'operator.tekton.dev/prune.schedule=* * * * *'
+
+  # deploy the database
+  $OC apply -n ${namespace} -f database/
+
+  # define the pipeline
+  $OC apply -n ${namespace} -f pipeline/
+
+  # expose the eventlistener
+  $OC create route edge el-library-shop -n ${namespace} \
+      --service=el-library-shop --insecure-policy=Redirect \
+      -o yaml
+done
